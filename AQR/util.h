7330658d1208c52280ea8eea4cf373c91c0743b7
Baiyu Huang <baiyuhua@usc.edu>
2017-08-08 16:30:30 -0700

    util.h

---
+++ AQR/util.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <string>
+#include <sstream>
+#include <iostream>
+
+//fill data from the fills file's line
+struct fillData
+{
+	uint64_t timeStamp;
+	std::string name;
+	double price;
+	int fillSize;
+	std::string action;
+};
+//price data from the prices files
+struct priceData
+{
+	uint64_t timeStamp;
+	std::string name;
+	double price;	
+};
+
+//keep track of spending and profit from each symbol
+class CompanyStock{
+public:
+	std::string symbol = "";
+	double investment;
+	int fillOwned;
+//public:
+	CompanyStock(){
+		symbol = "";
+		investment = 0;
+		fillOwned = 0;
+	}
+
+	CompanyStock(std::string name){
+		symbol = name;
+		investment = 0;
+		fillOwned = 0;
+	}
+	std::string getName(){
+		return symbol;
+	}
+	void Buy(int fillSize, double price){
+		investment += fillSize*price;
+		fillOwned += fillSize;
+	}
+	void Sell(int fillSize, double price){
+		investment -= fillSize*price;
+		fillOwned -= fillSize;
+	}
+	double calculatePNL(double currPrice){
+		return ((currPrice*fillOwned)-investment);
+	}
+};
+
+std::string delimit(int& start, char delimiter, std::string str){
+	std::string result = "";
+	for(int i = start; i < str.size(); i++){
+		if(str[i] != delimiter){
+			result+=str[i];
+		}
+		else{
+			start = i;
+			break;
+		}
+	}
+	return result;
+}
+
+uint64_t stringTouint64_t(std::string str){
+	uint64_t value;
+	std::istringstream iss(str);
+	iss >> value;
+	return value;
+}
